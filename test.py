02f16497a29526dab2d88a54690a9ab21246a6df
dsazykin <daniel.sazykin@gmail.com>
2025-10-11 19:06:36 +0200

    added new model

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import torchvision.transforms as T
 import mediapipe as mp
 
 # Create inference session
-session = ort.InferenceSession("gesture_model.onnx", providers=["CPUExecutionProvider"])
+session = ort.InferenceSession("gesture_model.onnx_v2", providers=["CPUExecutionProvider"])
 
 # Print input/output details
 print("Model inputs:", session.get_inputs())
