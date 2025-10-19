cd7018aa26d5032be2e49586cb60b4c27f23fcb4
dsazykin <daniel.sazykin@gmail.com>
2025-10-19 14:37:46 +0200

    pi will now try to connect to multiple different ips until it manages to connect to the laptop

--- TcpTest/PiClient.py
+++ TcpTest/PiClient.py
@@ -1,10 +1,25 @@
 import socket, time
 
-LAPTOP_IP = "192.168.178.16"
+possible_ips = [
+    "192.168.5.2",     # Ethernet static IP
+    "192.168.178.16"   # Daniel Home Local IP
+]
 PORT = 9000
 
+connected = False
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((LAPTOP_IP, PORT))
+
+while(not connected):
+    for ip in possible_ips:
+        try:
+            print(f"Trying {ip}...")
+            s.connect((ip, PORT))
+            print(f"Connected to {ip}")
+            connected = True
+            break
+        except:
+            continue
 
 def send_gesture(gesture):
     s.sendall(gesture.encode() + b"\n")
