1ee9f9dbdaf58a2c5813e857f49254a001c91625
BasM <b.p.mulder@student.utwente.nl>
2025-10-30 15:28:18 +0100

    the password gesture json is reset after submit

--- Pi/DeviceControl.py
+++ Pi/DeviceControl.py
@@ -180,6 +180,8 @@ def recognize_gestures():
     finally:
         empty_frame = np.zeros((480, 640, 3), dtype=np.uint8)
         cv2.imwrite(FRAME_PATH, empty_frame)  # Clear the frame
+        with open(JSON_PATH, 'w') as f:
+            json.dump({"gesture": "None"}, f)
 
 # Watch for mapping updates (using inotify)
 def watch_for_mapping_updates_inotify():
