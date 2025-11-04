230ce32d9965739837d0cebe03bcafe205989a38
dsazykin <daniel.sazykin@gmail.com>
2025-11-04 12:23:55 +0100

    webserver no longer runs in development mode

--- Pi/WebServer.py
+++ Pi/WebServer.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 
 from Pi.webserver import create_app
 from dotenv import load_dotenv
+from waitress import serve
 
 app = create_app()
 
-if __name__ == "__main__":  
-    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)
     load_dotenv()
